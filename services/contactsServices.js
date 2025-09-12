b77734cb109c3ac518baf5de8e8ffedc8d8c63a7
graf <oldestgraf@gmail.com>
2025-09-12 15:22:34 +0200

    Add contactsServices from HW1

---
+++ services/contactsServices.js
@@ -0,0 +1,43 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { nanoid } from "nanoid";
+
+export const contactsPath = path.resolve( "db", "contacts.json");
+
+async function readContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+}
+
+async function writeContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
+}
+
+export async function listContacts() {
+  return await readContacts();
+}
+
+export async function getContactById(contactId) {
+  const contacts = await readContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((c) => c.id === contactId);
+  if (index === -1) return null;
+  const [deleted] = contacts.splice(index, 1);
+  await writeContacts(contacts);
+  return deleted;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await readContacts();
+  const newContact = {
+    id: nanoid(), name, email, phone,
+  };
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
